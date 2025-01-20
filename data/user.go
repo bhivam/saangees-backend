b8401e9277ba3076928d38744237ff2d865daac4
bhivam <shivamkajaria@gmail.com>
2025-01-20 12:21:31 -0500

    removed autoincrement user ids

--- data/user.go
+++ data/user.go
@@ -7,7 +7,7 @@ import (
 var AnonymousUser = &User{}
 
 type User struct {
-	ID        int64     `gorm:"primaryKey;autoIncrement"       json:"id"`
+	ID        int64     `gorm:"primaryKey"                     json:"id"`
 	Name      string    `gorm:"type:text;not null"             json:"name"`
 	Email     string    `gorm:"type:text;not null;uniqueIndex" json:"email"`
 	Hash      string    `gorm:"type:text;not null"             json:"hash"`
@@ -29,4 +29,3 @@ type UserStore interface {
 func (user *User) IsAnonymous() bool {
 	return user == AnonymousUser
 }
-
